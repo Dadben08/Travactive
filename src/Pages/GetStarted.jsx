955ea08430749fc39ede67213d1812fe86f80b9a
Dadben08 <Dadben08@gmail.comcleargit config --global user.email Dadben08@gmail.com>
2025-12-17 15:14:33 +0100

    latest commit

--- src/Pages/GetStarted.jsx
+++ src/Pages/GetStarted.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Logo from "../assets/TravactiveLogo.png";
 import RightImage from "../assets/GetStartedImg.jpg";
 import { Link } from "react-router-dom";
-import { FaArrowRight, FaUser } from "react-icons/fa";
+import UserGradIcon from "../assets/userGraduate.png";
+import HealthTravel from "../assets/healthiconsTravel.png";
 
 const GetStarted = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      
       {/* Main Layout Container */}
       <div
         className="
@@ -26,25 +26,19 @@ const GetStarted = () => {
           gap-12
         "
       >
-        
         {/* Left side */}
-       <div className="flex-1 flex flex-col items-start text-left gap-6">
+        <div className="flex-1 flex flex-col items-center text-left gap-6">
+          <Link to="/" className="absolute top-8 left-36 z-10">
+            <img
+              src={Logo}
+              alt="Travactive Logo"
+              className="w-32 h-auto cursor-pointer items-center"
+            />
+          </Link>
 
-        <Link
-  to="/"
-  className="absolute top-6 left-6 z-10"
->
-  <img
-    src={Logo}
-    alt="Travactive Logo"
-    className="w-32 h-auto cursor-pointer"
-  />
-</Link>
-
-  
-  {/* Heading */}
-  <h2
-  className="
+          {/* Heading */}
+          <h2
+            className="
     w-[478px] h-[36px]
     font-[Outfit]
     font-semibold
@@ -54,86 +48,110 @@ const GetStarted = () => {
     text-[#212322]
     opacity-100
   "
->
-  Let’s Personalize Your Travel Experience
-</h2>
+          >
+            Let’s Personalize Your Travel Experience
+          </h2>
 
+          {/* Paragraph */}
+          <p className="text-[rgba(51,51,51,0.72)] w-[478px] h-[72px] font-[Inter] font-medium text-[16px] leading-[24px] tracking-normal">
+            Choose the path that fits you—whether you’re a student seeking
+            study-abroad insights or a traveler exploring global updates.
+          </p>
 
-  {/* Paragraph */}
-  <p
-  className="text-[rgba(51,51,51,0.72)] w-[478px] h-[72px] font-[Inter] font-medium text-[16px] leading-[24px] tracking-normal"
->
-  Choose the path that fits you—whether you’re a student seeking study-abroad
-  insights or a traveler exploring global updates.
-</p>
-
-
-  {/* Buttons Container */}
-  <div
-  className="
+          {/* Buttons Container */}
+          <div
+            className="
     flex flex-col
     w-[478px] h-[248px]
     gap-[24px]
     mt-4
   "
->
-  <a
-    href="#"
-    className="
-      flex items-center justify-center gap-2
+          >
+            {/* Button 1 */}
+            <a
+              href="#"
+              className="
+      flex flex-col items-start
       flex-1
-      px-6 py-3
+      px-6 py-4
       bg-[#FFFFFF]
       text-[#212322]
       rounded-[8px]
       font-semibold
       shadow-[0_4px_12px_0_#0000001A]
-      hover:bg-[#03504C]
+      hover:bg-[#03B5AA]
       transition
     "
-  >
-    <FaUser />
-    Join as Traveler
-  </a>
+            >
+              <img
+                src={UserGradIcon}
+                alt="Graduate Icon"
+                className="w-[24px] h-[24px] mb-2"
+              />
+              <h4
+                className="
+    w-[446px] h-[17px] font-[Inter] font-semibold text-[14px] leading-[17px] tracking-normal opacity-100"
+              >
+                As a Student
+              </h4>
 
-  <a
-    href="#"
-    className="
-      flex items-center justify-center gap-2
+              <p
+                className="
+    w-[446px] h-[15px] font-[Inter] font-normal text-[12px] leading-[15px] tracking-normal text-gray-600 opacity-100
+  "
+              >
+                seeking study-abroad insights.
+              </p>
+            </a>
+
+            {/* Button 2 */}
+            <a
+              href="#"
+              className="
+      flex flex-col items-start
       flex-1
-      px-6 py-3
+      px-6 py-4
       bg-[#FFFFFF]
       text-[#212322]
       rounded-[8px]
       font-semibold
       shadow-[0_4px_12px_0_#0000001A]
-      hover:bg-[#FF6A45]
+      hover:bg-[#FFDE59]
       transition
     "
-  >
-    <FaArrowRight />
-    Learn More
-  </a>
-</div>
-
-</div>
-
+            >
+              <img
+                src={HealthTravel}
+                alt="Health Travel Icon"
+                className="w-[24px] h-[24px] mb-2"
+              />
+              <h4 className="w-[446px] h-[17px] font-[Inter] font-semibold text-[14px] leading-[17px] tracking-normal opacity-100">
+                As a Traveler
+              </h4>
+              <p
+                className="
+    w-[446px] h-[15px] font-[Inter] font-normal text-[12px] leading-[15px] tracking-normal text-gray-600 opacity-100
+  "
+              >
+                exploring global updates.
+              </p>
+            </a>
+          </div>
+        </div>
 
         {/* Right side */}
         <div className="flex-1 flex justify-center items-center mr-[-46px]">
-  <img
-    src={RightImage}
-    alt="Get Started Illustration"
-    className="
+          <img
+            src={RightImage}
+            alt="Get Started Illustration"
+            className="
       w-[730px] h-[918px]
       rounded-[18px]
       opacity-100
       object-cover
     "
-  />
-</div>
-
-
+          />
+        </div>
       </div>
     </div>
   );
